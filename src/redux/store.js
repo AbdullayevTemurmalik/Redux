e1542a52f42fd0cfc2411e3b12079791b6e6a322
AbdullayevTemurmalik <tmaq77@gamil,com>
2025-10-18 15:31:48 +0500

    Redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { counterSlice } from "./counterSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    laylo: { counterSlice },
+  },
 });
